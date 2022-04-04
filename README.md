c7b6516227a8c5a5286e36049b11c69aa6aa0a4b
Sithembiso Sangweni <sithembiso72@gmail.com>
2022-04-04 16:55:30 +0200

    p
    push

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Electronjs
+https://www.electronjs.org/docs/latest
 # install 
 npm install --seve-dev electron@4.1.4
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const {app, BrowserWindow } = require('electron');
 
 function createWindows(){
-    let appWindow = new BrowserWindow();
+    let appWindow = new BrowserWindow({
+        width: 600,
+        height:800,
+        center: true,
+        minWidth: 300,
+        show:false,
+    });
     //load brouser
         //appWindow.loadURL('https://cmdbanner.io');
     // load file
         appWindow.loadFile('./index.html');
+
+        appWindow.on('closed', () => {
+            appWindow = null
+        });
+
+        appWindow.once('ready-to-show', ()=>{
+            appWindow.show();
+        });
 }
 
 app.on('ready', createWindows);
